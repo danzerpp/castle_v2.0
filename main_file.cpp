f4d311f63c24de176458d08d68f1d885c91e1b24
Dominik Dziabas <dominik199811@gmail.com>
2020-09-10 15:15:52 +0200

    ViewChanges

--- main_file.cpp
+++ main_file.cpp
@@ -27,6 +27,7 @@ Place, Fifth Floor, Boston, MA  02110 - 1301  USA
 #include <stdlib.h>
 #include <stdio.h>
 #include "myCube.h"
+#include "Bridge.h"
 #include "constants.h"
 #include "allmodels.h"
 #include "lodepng.h"
@@ -294,6 +295,28 @@ void kostka(glm::mat4 P, glm::mat4 V, glm::mat4 M) {
 	glDisableVertexAttribArray(spColored->a("color"));
 }
 
+void drawBridge(glm::mat4 P, glm::mat4 V, glm::mat4 M) 
+{
+	spColored->use(); //Aktywuj program cieniujący
+	M = glm::rotate(M, 44.0f, glm::vec3(0.0f, 0.0f, 1.0f));
+	M = glm::translate(M, glm::vec3(0.0f, 1.0f, -1.0f));
+	glUniformMatrix4fv(spColored->u("P"), 1, false, glm::value_ptr(P)); //Załaduj do programu cieniującego macierz rzutowania
+	glUniformMatrix4fv(spColored->u("V"), 1, false, glm::value_ptr(V)); //Załaduj do programu cieniującego macierz widoku
+	glUniformMatrix4fv(spColored->u("M"), 1, false, glm::value_ptr(M)); //Załaduj do programu cieniującego macierz modelu
+
+
+	glEnableVertexAttribArray(spColored->a("vertex"));
+	glVertexAttribPointer(spColored->a("vertex"), 4, GL_FLOAT, false, 0, myBridgeVertices); //Współrzędne wierzchołków bierz z tablicy birdVertices
+
+	glEnableVertexAttribArray(spColored->a("color"));
+	glVertexAttribPointer(spColored->a("color"), 4, GL_FLOAT, false, 0, myCubeColors); //Współrzędne wierzchołków bierz z tablicy birdColors
+
+	glDrawArrays(GL_TRIANGLES, 0, myBridgeVertexCount);
+
+	glDisableVertexAttribArray(spColored->a("vertex"));
+	glDisableVertexAttribArray(spColored->a("color"));
+}
+
 void texKostka(glm::mat4 P, glm::mat4 V, glm::mat4 M) {
 	//Tablica ta raczej powinna znaleźć się w pliku myCube.h, ale umieściłem ją tutaj, żeby w tej procedurze zawrzeć (prawie) całe rozwiązanie zadania
 	//Reszta to wczytanie tekstury - czyli kawałki kodu, które trzeba przekopiować ze slajdów
@@ -425,6 +448,9 @@ void drawScene(GLFWwindow* window, float angle_x, float angle_y) {
 
 
 	drawGround(P, V, M);
+	M = glm::mat4(1.0f);
+	drawBridge(P, V, M);
+
 	//kostka(P, V, M);
 	
 	//MUR
